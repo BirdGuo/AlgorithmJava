198de71d847bdb03ef37a0cd07988e3d7dc63bfb
gxw2 <gxw603004002@126.com>
2017-09-20 16:24:22 +0800

    添加素数筛选法

--- src/top/birdguo/algorithm/problem7/Problem7.java
+++ src/top/birdguo/algorithm/problem7/Problem7.java
@@ -28,22 +28,63 @@ public class Problem7 {
 				primeLoc = (6 * (((i + 1) / 2) - 1) - 1);
 			}
 			if (PrimeNumCount.isPrime(primeLoc)) {
-//				System.out.println(primes.size() + "   " + i + "   " + primeLoc);
+				// System.out.println(primes.size() + " " + i + " " + primeLoc);
 				primes.add(primeLoc);
 			}
 
 		}
 
-
 		return primes;
 	}
 
+	/**
+	 * 筛选法
+	 * 
+	 * @param maxNum
+	 * @return
+	 */
+	public static boolean[] isPrime(int maxNum) {
+
+		boolean[] primes = new boolean[maxNum + 1];
+
+		for (int i = 0; i < primes.length; i += 2) {
+			primes[i] = false;
+		}
+
+		for (int i = 1; i < primes.length; i += 2) {
+			primes[i] = true;
+		}
+
+		primes[1] = false;
+
+		for (int i = 3; i <= Math.sqrt(maxNum); i += 2) {
+			if (primes[i])
+				for (int j = i + i; j <= maxNum; j += i)
+					primes[j] = false;
+		}
+
+		primes[2] = true;
+
+		return primes;
+
+	}
+
 	public static void main(String[] args) {
-		ArrayList<Long> createPrime = createPrime(100000000L);
-//		System.out.println(createPrime);
-		System.out.println(createPrime.get(createPrime.size() - 1));
-		//
-		// System.out.println(createPrime(10001L));
+
+		long begin = System.currentTimeMillis();
+		// ArrayList<Long> createPrime = createPrime(2000000L);
+
+		boolean[] primes = isPrime(100000000);
+		for (int i = 0; i < primes.length; i++) {
+			if (primes[i]) {
+				System.out.println(i);
+			}
+		}
+
+		long end = System.currentTimeMillis();
+		System.out.println();
+		// System.out.println(createPrime.get(createPrime.size() - 1));
+		System.out.println((end - begin));
 	}
 
 }
