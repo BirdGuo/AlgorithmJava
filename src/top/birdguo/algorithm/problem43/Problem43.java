7d8a625798567e2ace2b3adb890a005bc894c956
gxw2 <gxw603004002@126.com>
2017-11-10 16:51:04 +0800

    添加44题

--- src/top/birdguo/algorithm/problem43/Problem43.java
+++ src/top/birdguo/algorithm/problem43/Problem43.java
@@ -1,13 +1,47 @@
 package top.birdguo.algorithm.problem43;
 
+import java.util.ArrayList;
+
+import top.birdguo.algorithm.prime.PrimeNumCount;
+import top.birdguo.algorithm.problem41.Problem41;
+
 public class Problem43 {
-	
+
 	public static void main(String[] args) {
-		
+		solution();
 	}
-	
+
 	public static void solution() {
-		
+
+		char[] chs = { '9', '8', '7', '6', '5', '4', '3', '2', '1', '0' };
+		ArrayList<String> nums = new ArrayList<String>();
+
+		Problem41.permutation(chs, 0, nums);
+
+		Long result = 0L;
+		for (String num : nums) {
+			String sub1 = num.substring(1, 4);
+			String sub2 = num.substring(2, 5);
+			String sub3 = num.substring(3, 6);
+			String sub4 = num.substring(4, 7);
+			String sub5 = num.substring(5, 8);
+			String sub6 = num.substring(6, 9);
+			String sub7 = num.substring(7, 10);
+
+			if (!PrimeNumCount.isPrime(Integer.valueOf(sub1)) && !PrimeNumCount.isPrime(Integer.valueOf(sub2))
+					&& !PrimeNumCount.isPrime(Integer.valueOf(sub3)) && !PrimeNumCount.isPrime(Integer.valueOf(sub4))
+					&& !PrimeNumCount.isPrime(Integer.valueOf(sub5)) && !PrimeNumCount.isPrime(Integer.valueOf(sub6))
+					&& !PrimeNumCount.isPrime(Integer.valueOf(sub7))) {
+
+				// System.out.println(num + " ");
+				result = result + Long.valueOf(num);
+
+			}
+
+		}
+
+		System.out.println("result:" + result);
+
 	}
 
 }
---
+++ src/top/birdguo/algorithm/problem44/Problem44.java
@@ -0,0 +1,9 @@
+package top.birdguo.algorithm.problem44;
+
+public class Problem44 {
+	
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
