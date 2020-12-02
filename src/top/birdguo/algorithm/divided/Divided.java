93edee14c1681b95f9066693e90a1920c6379d46
birdGuo <603004002@qq.com>
2020-12-02 22:38:38 +0800

    添加第五题思路

--- src/top/birdguo/algorithm/divided/Divided.java
+++ src/top/birdguo/algorithm/divided/Divided.java
@@ -33,9 +33,18 @@ public class Divided {
 
 	}
 
+	/**
+	 * 首先获取两个数的最大公约数，比如 6与8，最大公约数为2，最小公倍数为（6/2）*8 = 24
+	 * 问题再
+	 */
+	public static void dividedNum(){}
+
 	public static void main(String[] args) {
 		int a = 1;
-		for (int i = 1; i < 20; i++) {
+		for (int i = 1; i < 21; i++) {
+
+			System.out.println("===i="+i);
+
 			a = getMinMult(a, i);
 		}
 		System.out.println(a);
